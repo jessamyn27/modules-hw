e9669693fa75145226b50e4dce17f8d571b2b22f
Jessamyn McTwigan <jessamyn.mctwigan@gmail.com>
2018-07-08 21:42:33 -0500

    ade call and answer

---
+++ index.js
@@ -0,0 +1,11 @@
+const my = require('./car');
+
+console.log(my, ' this is the car');
+
+console.log(my.color, ' this is the color');
+
+console.log(my.convertible, ' this is my convertible');
+
+console.log(my.speedAccelerate, ' this is the accelerated speed');
+
+console.log(my.speedDecelerate, ' this is the decelerated speed');
